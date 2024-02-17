a001e3f7e30b01ee33abb39631bc0ff0900c89fa
krishchuk <ms.den.best@mail.ru>
2024-02-17 21:29:43 +0500

    add config func

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from configparser import ConfigParser
+
 EMPLOYERS_ID_LIST = [
     "2180",
     "15478",
@@ -11,3 +13,17 @@ EMPLOYERS_ID_LIST = [
     "64174"
 ]
 HH_URL = "https://api.hh.ru/vacancies"
+
+
+def config(filename="config.ini", section="postgresql"):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            'Section {0} is not found in the {1} file.'.format(section, filename))
+    return db
